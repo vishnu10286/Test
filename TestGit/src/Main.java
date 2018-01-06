86f648ba2e3ff002596f8d9e5a6399346d8e4300
selvi <selvi@selvi-PC>
2018-01-06 16:02:00 +0530

    test commit

---
+++ TestGit/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				 int h = 0;
+				 h++;
+		float a=55;
+		System.out.println(a);
+		long b= (long) a;
+		float c = b;
+		System.out.println(c);
+		System.out.println(b);
+		char s='+';
+		System.out.println(s);
+		short q=(short) s;
+		System.out.println(q);
+		int x= q;
+		System.out.println(x);
+		byte y=(byte) x;
+		System.out.println(y);
+		System.out.println(s);
+
+	}
+
+}
